7751555f1afb9afaa697fa0a7bf5e8e8d1c0766a
BoyoungYun <index_1234@naver.com>
2023-11-07 11:30:40 +0900

    feat: Apply naver map and display marker in mapComponent

--- client/src/components/MapComponent/MapComponent.tsx
+++ client/src/components/MapComponent/MapComponent.tsx
@@ -1,23 +1,56 @@
-import { GoogleMap, LoadScriptNext } from "@react-google-maps/api";
-import { useMemo } from "react";
-import { DirectionComponent } from "../DirectionComponent";
+import React, { useEffect } from "react";
 import { Wrapper } from "./styles";
 
 export default function MapComponent() {
-  const center = useMemo(() => ({ lat: 33.499648, lng: 126.531275 }), []);
+  const locations = [
+    {
+      lat: 33.252479,
+      lng: 126.623535,
+    },
+    {
+      lat: 33.25097,
+      lng: 126.62076,
+    },
+    {
+      lat: 33.232008,
+      lng: 126.314855,
+    },
+    {
+      lat: 33.233214,
+      lng: 126.308798,
+    },
+    {
+      lat: 33.290313,
+      lng: 126.352014,
+    },
+    {
+      lat: 33.264617,
+      lng: 126.370525,
+    },
+    {
+      lat: 33.391216,
+      lng: 126.23545,
+    },
+  ];
+  useEffect(() => {
+    const initMap = () => {
+      const map = new naver.maps.Map("map", {
+        center: new naver.maps.LatLng(33.252479, 126.623535),
+        zoom: 11,
+      });
+      locations.map((item) => {
+        new naver.maps.Marker({
+          position: new naver.maps.LatLng(item.lat, item.lng),
+          map,
+        });
+      });
+    };
+    initMap();
+  }, []);
+
   return (
     <Wrapper>
-      <LoadScriptNext
-        googleMapsApiKey={`${import.meta.env.VITE_GOOGLE_API_KEY}`}
-      >
-        <GoogleMap
-          zoom={13}
-          center={center}
-          mapContainerClassName="map-container"
-        >
-          <DirectionComponent />
-        </GoogleMap>
-      </LoadScriptNext>
+      <div id="map" className="map-container"></div>
     </Wrapper>
   );
 }
